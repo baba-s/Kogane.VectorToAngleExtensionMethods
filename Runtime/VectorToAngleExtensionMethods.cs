3e0307da0feb884df2e0106751e687e1f159bf96
baba-s <shotababa1206@gmail.com>
2022-10-15 09:13:46 +0900

    fix

--- Runtime/VectorToAngleExtensionMethods.cs
+++ Runtime/VectorToAngleExtensionMethods.cs
@@ -16,12 +16,12 @@ namespace Kogane
 
         public static Vector2 RadianToVector2( this float self )
         {
-            return new Vector2( Mathf.Cos( self ), Mathf.Sin( self ) );
+            return new( Mathf.Cos( self ), Mathf.Sin( self ) );
         }
 
         public static Vector2 DegreeToVector2( this float self )
         {
-            return new Vector2( Mathf.Cos( self * Mathf.Deg2Rad ), Mathf.Sin( self * Mathf.Deg2Rad ) );
+            return new( Mathf.Cos( self * Mathf.Deg2Rad ), Mathf.Sin( self * Mathf.Deg2Rad ) );
         }
     }
 }
